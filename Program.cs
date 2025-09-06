ac9278e623f3deb11e302e359521678922811894
Gabrovez <nadigar1506@mail.ru>
2025-09-06 12:45:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+
+            ////Console.WriteLine("Введите ваше имя");
+            //string? name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}");
+
+            //string name = "ANNA";
+            //int age = 18;
+            //string city = "volgograd";
+            //Console.WriteLine($"Ваше имя - {name}, ваш возраст {age},ваш город {city} ");
+
+
+            //Console.WriteLine("Введите ваш возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine(age);
+
+            //int number = 250;
+            //long number2 = number;
+            //Console.WriteLine(number2);
+
+            //bool alive = true;
+            //bool isDead = false;
+
+            //double iDouble = 4.5;
+            //float iFloat = 4.3F;
+            //decimal iDecimal = 3.4m;
+            //object fdf = 4;
+          
+            //var name = " Dfs";
+
+
+
+        }
+    }
+}
